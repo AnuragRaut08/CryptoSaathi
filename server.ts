98d5cb2f94d2d75ebb7a70d6deb02f342b5fe377
Anurag Tryambak Raut <120129433+AnuragRaut08@users.noreply.github.com>
2025-06-07 13:33:39 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 4000;
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     // Start the cron job
+    
     startPriceUpdater();
   });
 })();
